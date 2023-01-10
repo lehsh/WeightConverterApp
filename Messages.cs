de85ed7a0b588e685b41275d87a543581aee04d1
Shishkin Aleksey <ash911@mail.ru>
2023-01-10 20:13:45 +0300

    add conversion

--- Messages.cs
+++ Messages.cs
@@ -2,7 +2,9 @@
 {
     public class Messages
     {
+        public record AboutMessage(string About);
         public record StatusMessage(string Server, int Port);
-        public record InfoMessage(string Handler, string Discription);
+        public record InfoMessage(string Handler, string Description);
+        public record InfoMessages(List<InfoMessage> List);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using WeightConverterJsonAPI;
 using static WeightConverterApp.Messages;
 
@@ -21,41 +22,44 @@ app.MapGet("/status", async (context) =>
 
 app.MapGet("/info", async (context) =>
 {
-    InfoMessage statusInfo = new StatusMessage("GET /status", "Информация о сервере, номер порта")
+    InfoMessage infoStatus = new InfoMessage("GET /status", "Информация о сервере, номер порта");
+    InfoMessage infoConversion = new InfoMessage("POST /conversion", "Конвертация мер веса");
+    InfoMessages infoMessage = new InfoMessages(new List<InfoMessage> { infoStatus, infoConversion });
 
-    await context.Response.WriteAsJsonAsync(statusInfo);
+    await context.Response.WriteAsJsonAsync(infoMessage);
 });
 
 app.MapPost("/conversion", async (context) =>
 {
-MeasureOfWeight? measure = null;
-try
-{
-    measure = await context.Request.ReadFromJsonAsync<MeasureOfWeight>();
-    Console.WriteLine(measuresOfWeight);
-}
-catch { }
-
-if (measure is null)
-{
-    ErrorMessage error = new ErrorMessage("Invalid request paramters");
-    Console.WriteLine(error);
-    await context.Response.WriteAsJsonAsync(error);
-}
-else
-{
+    MeasureOfWeight? measure = null;
     try
     {
-        measuresOfWeight.ChangeValue(measure);
+        measure = await context.Request.ReadFromJsonAsync<MeasureOfWeight>();
         Console.WriteLine(measuresOfWeight);
-        await context.Response.WriteAsJsonAsync(measuresOfWeight);
     }
-    catch (Exception ex)
+    catch { }
+
+    if (measure is null)
     {
-        ErrorMessage error = new ErrorMessage($"Error during request processing: {ex.Message}");
+        ErrorMessage error = new ErrorMessage("Invalid request paramters");
         Console.WriteLine(error);
         await context.Response.WriteAsJsonAsync(error);
     }
-}
+    else
+    {
+        try
+        {
+            measuresOfWeight.ChangeValue(measure);
+            Console.WriteLine(measuresOfWeight);
+            await context.Response.WriteAsJsonAsync(measuresOfWeight);
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage error = new ErrorMessage($"Error during request processing: {ex.Message}");
+            Console.WriteLine(error);
+            await context.Response.WriteAsJsonAsync(error);
+        }
+    }
+});
 
 app.Run();
